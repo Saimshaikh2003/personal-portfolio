da80c29d019d9b7b46e1f90475a81d8cdd488fc1
SAIM SHAIKH <saimshaikh0003@gmail.com>
2024-11-08 19:40:50 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import React from "react";
 import Hero from "./Components/Hero";
 import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
-import AOS from "aos"
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import AOS from "aos";
